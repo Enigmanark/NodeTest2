faedbbcdbf2741ad16f5e9e3c290f381e5e871e9
Keleiron <johnathan.crocker@gmail.com>
2017-08-30 11:00:32 -0500

    First test done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var titleOfSite = "Solar's Node Test 2";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'titleOfSite' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'titleOfSite' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'titleOfSite' });
 });
 
 module.exports = router;
